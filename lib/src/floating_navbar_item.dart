53cd2f4a830eb195f30c9bce323c95cfed87a066
Saifallak <Saifallak@yahoo.com>
2020-06-07 02:15:57 +0200

    custom widget

--- lib/src/floating_navbar_item.dart
+++ lib/src/floating_navbar_item.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class FloatingNavbarItem {
   final String title;
   final IconData icon;
+  final Widget customWidget;
 
   FloatingNavbarItem({
     @required this.icon,
     @required this.title,
+    this.customWidget = const SizedBox(),
   });
 }
